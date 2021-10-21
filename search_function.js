b17e504179805af52abc0072941459d18f57c1a6
Leo Fielstette <88216682+syph1000@users.noreply.github.com>
2021-10-21 20:37:05 +0200

    Create search_function.js

---
+++ search_function.js
@@ -0,0 +1,14 @@
+//search funktion
+const f = document.getElementById("form");
+const q = document.getElementById("query");
+const google = "https://www.google.com/search?q=site%3A+";
+const site = "pagedart.com";
+
+function submitted(event) {
+  event.preventDefault();
+  const url = google + site + "+" + q.value;
+  const win = window.open(url, "_blank");
+  win.focus();
+}
+
+f.addEventListener("submit", submitted);
